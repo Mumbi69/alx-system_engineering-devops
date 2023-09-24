51575e422736e4521f9602f90b644d85767fad1b
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-24 23:44:51 +0000

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,6 @@
 # set up your client SSH configuration file so that you can
 # connect to a server without typing a password.
-# Set up server configuration with puppet
-file_line{'Set alias name':
+file_line{'Name':
   path => '/etc/ssh/ssh_config',
   line => 'Host localhost
     HostName localhost
