16b491764bb1aa4aa015e91b7c7c16e06a3d0d01
Mumbi69 <winfriendmumbi69@gmail.com>
2023-11-08 17:22:52 +0000

    debugging apache

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
 # Using tmux with strace to debug apache server
 exec { 'debug_apache':
-        command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-        provider => 'shell'
+        command  => 'grep -q 'phpp' /var/www/html/wp-settings.php',
 }
