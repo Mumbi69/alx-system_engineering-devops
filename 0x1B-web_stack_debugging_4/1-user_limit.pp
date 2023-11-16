c94b7bca47c08dab9a0f5b12781167482d60c122
Mumbi69 <winfriendmumbi69@gmail.com>
2023-11-17 00:42:39 +0300

    holberton user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # Giving access to holberton user
 exec { 'update_limits_conf':
-  command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 5000/; s/holberton soft nofile 4/holberton soft nofile 4000/" /etc/security/limits.conf',
+  command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile unlimited/; s/holberton soft nofile 4/holberton soft nofile unlimited/" /etc/security/limits.conf',
   provider => 'shell',
 }
 
